f4d9a71b53240e25b7af07c1e616d96353982e29
Nikhil Moorthy <122428374+nikki7817@users.noreply.github.com>
2024-02-08 10:19:33 +0530

    Minor Tweak

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function Search({ location, setLocation, getCurrentData, errorMessage }) {
+function Search({ location, setLocation, getCurrentData, errorMessage, c_weather }) {
   return (
     <>
       <h1 className="weatherapp animate__animated animate__jello">
         Weather Man
+
+      {c_weather && c_weather.weather && c_weather.weather.length > 0 && (
+      <span> <img src={`https://openweathermap.org/img/wn/${c_weather.weather[0].icon}.png`} /></span>
+      )}
+
       </h1>
       <div className="search">
         <input
