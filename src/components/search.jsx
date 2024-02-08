1d046eef91fe8c5a1ce3bbc34ddaa50ade3eb4f6
nikki7817 <nikhilmoorthy590@gmail.com>
2024-02-08 11:00:54 +0530

    Adding an icon

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -5,7 +5,13 @@ function Search({ location, setLocation, getCurrentData, errorMessage, c_weather
     <>
       <h1 className="weatherapp animate__animated animate__jello">
         Weather Man
+      {c_weather && c_weather.weather && c_weather.weather.length > 0 && (
+
+      <span> <img src={`https://openweathermap.org/img/wn/${c_weather.weather[0].icon}.png`} /></span>
+
+      )}
       </h1>
+
       <div className="search">
         <input
           className="animate__animated animate__jello"
